b95ec728fb88cdf73d73f92ae831c0ae1ebeab6b
Makel Grax <Makelmagus@gmail.com>
2020-07-07 13:45:05 -0300

    The sketch that I'll use to create the game.

---
+++ tateti_juego_v2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <tateti.h>
+
+main()
+{
+  //menu
+  draw_title();
+  select_AI_type();
+  cleanup_screen();
+  //game
+  draw_board();
+
+  while(winner() == 0)
+  {
+    player_turn();
+    draw_move();
+    if(winner() != 0)
+    {
+      goto end;
+    }
+    AI_turn();
+    draw_move();
+  }
+  //winner declared
+  end:
+  cleanup_screen();
+  draw_title();
+  declare_winner();
+  /* Idea:
+  *   if (winner() == 1)
+  *   {
+  *     printf("The human player won.");
+  *   } else
+  *   if (winner() == 2)
+  *   {
+  *     printf("The machine shall rise from this victory, and control the world!");
+  *   }
+  */
+}
