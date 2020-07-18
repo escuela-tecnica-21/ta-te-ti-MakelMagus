810c1ca41574a40f3ab34e25a0e447967de5afd4
Makel Grax <40347489+MakelMagus@users.noreply.github.com>
2020-07-18 19:35:25 -0300

    I actually did the thing of moving the functions BELOW the main() code. Currently thinking on how to get rid of "goto" code and how to replace "gotoxy".

--- tateti_juego.c
+++ tateti_juego.c
@@ -3,16 +3,60 @@
 #include <conio2.h>
 #include <string.h>
 
-//miscellaneous variables
-int counter=0;
-//graphic variables
-int base_x=0, base_y=0, cell_size=3
-int table_size = cell_size*3;
-int end_of_table_x = table_size + base_x;
-int end_of_table_y = table_size + base_y;
-//game variables
-int cell[9], user_movement=0, winner=0, turn=0;
-//funciones
+//function declaration
+void user_movement_place(user_movement,cell_size){};
+void draw_horizontal_line(size,y_position){};
+void draw_vertical_line(size,x_position){};
+void write_table(x,y,cell_size){};
+int check_for_winner(cell[]){};
+int dumb_ai_movement(cell[]){};
+
+//juego
+int main() {
+
+  //miscellaneous variables
+  int counter=0;
+  //graphic variables
+  int base_x=0, base_y=0, cell_size=3
+  int table_size = cell_size*3;
+  int end_of_table_x = table_size + base_x;
+  int end_of_table_y = table_size + base_y;
+  //game variables
+  int cell[9], user_movement=0, winner=0, turn=0;
+
+  //sets the playing field
+  write_table(base_x,base_y,cell_size);
+
+
+  for (turn = 0; turn < 9; turn += 2){
+    if (check_for_winner(cell[]) == 0)
+    {
+      //ask the player for input
+      gotoxy(end_of_table_x+1,end_of_table_y+1);
+      scanf("%d",&user_movement);
+      //place the cross on the player input
+      user_movement_place(user_movement,cell_size,1);
+    } else
+    {
+      goto end;
+    }
+    if (check_for_winner(cell) == 0 && turn < 9)
+    {
+      user_movement_place(dumb_ai_movement(cell),cell_size,2);
+    } else
+    {
+      goto end;
+    }
+  }
+
+  end:
+  systemclear();
+  printf("And the winner is player %d",check_for_winner);
+  scanf("%d",&counter);
+  return 0;
+}
+
+//Functions defined here
 void user_movement_place(user_movement,cell_size) {
   int x,y,cell_mod;
   cell_mod = cell % 3;
@@ -44,7 +88,7 @@ void write_table(x,y,cell_size){
     draw_vertical_line(cell_size*3,x+cell_size*(i+1));
   }
 }
-int check_for_winner(cell[]) {
+int check_for_winner(cell[]){
   for (counter = 0; counter < 3; counter++){
     //horizontal check
     if( cell[ counter*3 ] == cell[ (counter*3) + 1 ] && cell[ counter*3 ] == cell[ counter*3 ] + 2){
@@ -77,41 +121,4 @@ int dumb_ai_movement(cell[]) {
     }
   }
   return random;
-}
-
-//juego
-int main() {
-
-  //set vectors to 0
-  for (counter = 0; counter < 9 ;counter++)
-  {
-    cell[counter] = 0;
-  }
-
-  write_table(base_x,base_y,cell_size);
-  //ask the player for input
-  for (turn = 0; turn < 9; turn += 2){
-    if (check_for_winner(cell) == 0)
-    {
-      gotoxy(end_of_table_x+1,end_of_table_y+1);
-      scanf("%d",&user_movement);
-      user_movement_place(user_movement,cell_size,1);
-    } else
-    {
-      goto end;
-    }
-    if (check_for_winner(cell) == 0 && turn < 9)
-    {
-      user_movement_place(dumb_ai_movement(cell),cell_size,2);
-    } else
-    {
-      goto end;
-    }
-  }
-
-  end:
-  systemclear();
-  printf("And the winner is player %d",check_for_winner);
-  scanf("%d",&counter);
-  return 0;
 }
